29849d83f10eb78f82e0fcad8e3ed3b04c7a90b5
Pandaaaa906 <ye.pandaaaa906@gmail.com>
2017-10-18 00:04:54 +0800

    asfd

--- workflow/models/process.py
+++ workflow/models/process.py
@@ -1,24 +1,35 @@
 # coding=utf-8
-from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from workflow.models.workflow import WorkFlow, WorkFlowNode
 from workflow.models.base_model import BaseModel
 
 
-class Process(BaseModel):
-    status_id = models.IntegerField(blank=True, null=True)
-
-    def push(self):
-        self.active_workflow.start(self)
-
-    def retract(self):
-        pass
-
+class BaseProcess(BaseModel):
     @property
     def active_workflow(self):
         return WorkFlow.objects.get(in_use=True,
                                     process_obj=self._meta.model,
                                     )
+
+
+class Process(BaseProcess):
+    workflow_node = models.ForeignKey(WorkFlowNode, blank=True, null=True, default=None)
+
+    def start(self):
+        if self.workflow_node is None:
+            self.workflow_node = WorkFlowNode.objects.get(workflow=self.active_workflow,
+                                                          node_type=WorkFlowNode.START_TYPE
+                                                          )
+        else:
+            pass
+
+    def proceed(self, user):
+        """判断用户权限"""
+
+
+    def retract(self):
+        """撤销提交"""
+        pass
 
 
 class ProcessLog(BaseModel):
--- workflow/models/workflow.py
+++ workflow/models/workflow.py
@@ -1,7 +1,8 @@
 # coding=utf-8
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
-from base_model import BaseModel
+from django.utils.translation import ugettext_lazy as _
+from workflow.models.base_model import BaseModel
 
 
 class WorkFlow(BaseModel):
@@ -9,20 +10,25 @@ class WorkFlow(BaseModel):
     in_use = models.BooleanField(default=False)
     process_obj = models.ForeignKey(ContentType)
 
-    def start(self, process_instance):
-        pass
-
 
 # TODO flow direction: forword, backward
 class WorkFlowNode(BaseModel):
     START_TYPE = "START"
     END_TYPE = "END"
+    USER_TYPE = "USER"
+    SYS_TYPE = "SYS"
     NODE_TYPE = (
-        (START_TYPE, "开始"),
-        (END_TYPE, "结束"),
+        (START_TYPE, _("开始")),
+        (END_TYPE, _("结束")),
+        (USER_TYPE, _("用户节点")),
+        (SYS_TYPE, _("系统节点")),
     )
     workflow = models.ForeignKey(WorkFlow)
-    ancestor_node = models.ForeignKey('self', blank=True, null=True)
-    descendant_node = models.ForeignKey('self', blank=True, null=True)
-
     node_type = models.CharField(max_length=100)
+
+
+class WorkFlowTransition(BaseModel):
+    workflow = models.ForeignKey(WorkFlow)
+    ancestor_node = models.ForeignKey(WorkFlowNode, blank=True, null=True)
+    descendant_node = models.ForeignKey(WorkFlowNode, blank=True, null=True)
+    transition_type
