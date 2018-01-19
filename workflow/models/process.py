63984e1246e016b4b7aec18c0eee0921516d6196
Pandaaaa <ye.pandaaaa906@gmail.com>
2018-01-19 18:13:27 +0800

    New Project

--- workflow/models/process.py
+++ workflow/models/process.py
@@ -1,7 +1,10 @@
 # coding=utf-8
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from workflow.models.workflow import WorkFlow, WorkFlowNode
 from workflow.models.base_model import BaseModel
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class BaseProcess(BaseModel):
@@ -11,7 +14,41 @@ class BaseProcess(BaseModel):
                                     process_obj=self._meta.model,
                                     )
 
+    class Meta:
+        abstract = True
 
+
+class BaseVoucher(BaseModel):
+
+    class Meta:
+        abstract = True
+
+    @property
+    def get_proceeding(self):
+        return Proceeding.objects.get(voucher_type=self._meta.model,
+                                      voucher_id=self.id
+                                      )
+
+
+class Voucher(BaseVoucher):
+
+    def submit(self):
+        """提交"""
+        pass
+
+    class Meta:
+        abstract = True
+
+
+class Proceeding(BaseModel):
+    voucher_type = models.ForeignKey(ContentType)
+    voucher_obj_id = models.PositiveIntegerField()
+    workflow_node = models.ForeignKey(WorkFlowNode, blank=True, null=True, default=None)
+
+
+
+
+# deprecated??
 class Process(BaseProcess):
     workflow_node = models.ForeignKey(WorkFlowNode, blank=True, null=True, default=None)
 
@@ -25,7 +62,7 @@ class Process(BaseProcess):
 
     def proceed(self, user):
         """判断用户权限"""
-
+        pass
 
     def retract(self):
         """撤销提交"""
